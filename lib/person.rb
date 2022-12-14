74af716207b59ff5fb121134a864d680d2ce9858
Keith Ma <keithchezzy@gmail.com>
2022-12-13 19:55:44 -0800

    tricky

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,27 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        @bank_account=25
+        @happiness = 8
+        @hygiene = 8
+    end
+   def happiness
+    @happiness.clamp(0,10)
+   end
+   def hygiene
+    @hygiene.clamp(0,10)
+   end
+   def happy?
+    self.happiness>7
+   end
+   def clean?
+    self.hygiene>7
+   end
+   def get_paid(amount)
+       self.bank_account += amount
+        "all about the benjamins"
+   end
+end
