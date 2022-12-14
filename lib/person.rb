30418a0057f650b956f151906ec6d146b9e89595
Keith Ma <keithchezzy@gmail.com>
2022-12-14 10:48:06 -0800

    tricky

--- lib/person.rb
+++ lib/person.rb
@@ -4,24 +4,55 @@ class Person
     attr_reader :name
     def initialize(name)
         @name = name
-        @bank_account=25
+        @bank_account =25
         @happiness = 8
         @hygiene = 8
     end
-   def happiness
-    @happiness.clamp(0,10)
-   end
-   def hygiene
-    @hygiene.clamp(0,10)
-   end
-   def happy?
-    self.happiness>7
-   end
-   def clean?
-    self.hygiene>7
-   end
-   def get_paid(amount)
-       self.bank_account += amount
+    def happiness
+        @happiness.clamp(0,10)
+    end
+    def hygiene
+        @hygiene.clamp(0,10)
+    end
+    def happy?
+        self.happiness>7
+    end
+    def clean?
+        self.hygiene>7
+    end
+    def get_paid(amount)
+        self.bank_account+=amount
         "all about the benjamins"
-   end
+    end
+    # writer methods
+    def take_bath
+        # @hygiene = @hygiene + 4
+        # self.hygiene=(self.hygiene + 4)
+        self.hygiene+=4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    def work_out
+        self.hygiene-=3
+        self.happiness +=2
+        '♪ another one bites the dust ♫'
+    end
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness +=3
+        "Hi #{friend.name}! It's #{name}. How are you?"
+    end
+    def start_conversation(friend, topic)
+        case topic
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness += 1
+            friend.happiness +=1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
